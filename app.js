11d29bbc27d4216bf673a37be130d5390866be96
Aman-Giri01 <amankumargiri2022@gmail.com>
2025-01-26 20:04:13 +0530

    separating routes in separate modules

--- app.js
+++ app.js
@@ -1,99 +1,19 @@
-import { readFile ,writeFile} from "fs/promises";
-import path from "path";
-import crypto from "crypto";
 import express from "express";
+import {shortenerRoots} from "./routes/shortner.routes.js";
 
 
 const app=express();
 
 const PORT=process.env.PORT ||3000;
 
-const DATA_FILE=path.join("data","links.json");
+
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
+app.use(shortenerRoots);
 
 
 
-
-const loadLinks=async()=>{
-    try{
-
-        const data=await readFile(DATA_FILE,'utf-8');
-        return JSON.parse(data);
-
-    }
-    catch(err)
-    {
-      if(err.code==="ENOENT")
-      {
-        await writeFile(DATA_FILE,JSON.stringify({}));
-        return{};
-      }
-      throw err;
-    }
-
-};
-
-const saveLinks=async(links)=>{
-    await writeFile(DATA_FILE,JSON.stringify(links));
-}
-
-app.get("/",async(req,res)=>{
-    try {
-        const file=await readFile(path.join("views","index.html"));
-        const links=await loadLinks();
-
-        const content=file.toString().replaceAll("{{shortend_urls}}",Object.entries(links).map(
-            ([shortCode,url])=>`<li><a href="/${shortCode}" target="_blank"> ${req.host}/${shortCode}</a> -> ${url}</li>`
-        ).join(""));
-
-        return res.send(content);;
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Internal Server Error");
-    }
-});
-
-app.post("/",async(req,res)=>{
-    try {
-        const {url,shortCode}=req.body;
-        const finalShortCode=shortCode ||crypto.randomBytes(4).toString("hex"); //to check duplicate data
-
-        const links=await loadLinks();
-
-            if(links[finalShortCode])
-            {
-                return res.status(400).send("Short code already exists. Please choose another");
-
-            }
-
-        links[finalShortCode]=url;
-        await saveLinks(links);
-        return res.redirect("/");
-
-
-    } catch (error) {
-        return res.status(500).send("Internal Server Error");
-        
-    }
-});
-
-app.get("/:shortCode",async(req,res)=>{
-    try {
-        const {shortCode}=req.params;
-        const links=await loadLinks();
-
-        if(!links[shortCode]) return res.status(404).send("404 error occurred.");
-
-        return res.redirect(links[shortCode]);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Internal server error");
-        
-    }
-});
 
 
 app.listen(3000,()=>console.log('listening server with port no.',PORT));
---
+++ routes/shortner.routes.js
@@ -0,0 +1,92 @@
+import crypto from "crypto";
+import { readFile ,writeFile} from "fs/promises";
+import path from "path";
+import {Router} from "express";
+
+const router=Router();
+
+const DATA_FILE=path.join("data","links.json");
+const loadLinks=async()=>{
+    try{
+
+        const data=await readFile(DATA_FILE,'utf-8');
+        return JSON.parse(data);
+
+    }
+    catch(err)
+    {
+      if(err.code==="ENOENT")
+      {
+        await writeFile(DATA_FILE,JSON.stringify({}));
+        return{};
+      }
+      throw err;
+    }
+
+};
+
+const saveLinks=async(links)=>{
+    await writeFile(DATA_FILE,JSON.stringify(links));
+}
+
+router.get("/",async(req,res)=>{
+    try {
+        const file=await readFile(path.join("views","index.html"));
+        const links=await loadLinks();
+
+        const content=file.toString().replaceAll("{{shortend_urls}}",Object.entries(links).map(
+            ([shortCode,url])=>`<li><a href="/${shortCode}" target="_blank"> ${req.host}/${shortCode}</a> -> ${url}</li>`
+        ).join(""));
+
+        return res.send(content);;
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Internal Server Error");
+    }
+});
+
+router.post("/",async(req,res)=>{
+    try {
+        const {url,shortCode}=req.body;
+        const finalShortCode=shortCode ||crypto.randomBytes(4).toString("hex"); //to check duplicate data
+
+        const links=await loadLinks();
+
+            if(links[finalShortCode])
+            {
+                return res.status(400).send("Short code already exists. Please choose another");
+
+            }
+
+        links[finalShortCode]=url;
+        await saveLinks(links);
+        return res.redirect("/");
+
+
+    } catch (error) {
+        return res.status(500).send("Internal Server Error");
+        
+    }
+});
+
+router.get("/:shortCode",async(req,res)=>{
+    try {
+        const {shortCode}=req.params;
+        const links=await loadLinks();
+
+        if(!links[shortCode]) return res.status(404).send("404 error occurred.");
+
+        return res.redirect(links[shortCode]);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Internal server error");
+        
+    }
+});
+
+// export default router;
+
+// Named exports
+
+export const shortenerRoots=router;
