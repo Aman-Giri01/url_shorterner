34cca1fba37f5929f70247bb63a622bacb56caf2
Aman-Giri01 <amankumargiri2022@gmail.com>
2025-04-05 11:21:52 +0530

    Stop tracking folder_name

--- models/shortener.model.js
+++ models/shortener.model.js
@@ -1,30 +1,3 @@
-// import { readFile ,writeFile} from "fs/promises";
-// import path from "path";
-
-// const DATA_FILE=path.join("data","links.json");
-// export const loadLinks=async()=>{
-//     try{
-
-//         const data=await readFile(DATA_FILE,'utf-8');
-//         return JSON.parse(data);
-
-//     }
-//     catch(err)
-//     {
-//       if(err.code==="ENOENT")
-//       {
-//         await writeFile(DATA_FILE,JSON.stringify({}));
-//         return{};
-//       }
-//       throw err;
-//     }
-
-// };
-
-// export const saveLinks=async(links)=>{
-//     await writeFile(DATA_FILE,JSON.stringify(links));
-// }
-
 import { dbClient } from "../config/db-client.js";
 import { env } from "../config/env.js";
 
--- routes/shortner.routes.js
+++ routes/shortner.routes.js
@@ -3,18 +3,6 @@ import {Router} from "express";
 
 const router=Router();
 
-router.get("/report",(req,res)=>{
-    const student=[
-    {name:"Aman",class:"10",roll:4},
-    {name:"Ram",class:"10",roll:44},
-    {name:"Kunal",class:"10",roll:32},
-    {name:"Ramesh",class:"10",roll:25}
-
-];
-    res.render("report",{student});
-
-});
-
 router.get("/", getShortenerPage);
 
 router.post("/",postURLShortener);
